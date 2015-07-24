4154eb6249429cf6a543a0aa8ecc581cb971f8f1
unknown <fmelle002@FMELLE00227.us.nam.ad.pwcinternal.com>
2015-07-24 11:18:20 -0700

    Updates survey questions

--- app/js/services.js
+++ app/js/services.js
@@ -25,62 +25,62 @@ app.factory("SurveyData", [function () {
 		"peopleData": [
 			{
 				"questionNo": 1,
-				"question": "Important strategic and operational decisions are quickly translated into action ",
+				"question": '"Important strategic and operational decisions are quickly translated into action"',
 				"options": ["Agree", "Disagree"]
 			}, 
 			{
 				"questionNo": 2,
-				"question": "We are disciplined in focusing our efforts on where we can win",
+				"question": '"We are disciplined in focusing our efforts on where we can win"',
 				"options": ["Agree", "Disagree"]
 			},  
 			{
 				"questionNo": 3,
-				"question": "Once made, decisions are rarely second-guessed ",
+				"question": '"Once made, decisions are rarely second-guessed"',
 				"options": ["Agree", "Disagree"]
 			}, 
 			{
 				"questionNo": 4,
-				"question": "We have a distinctive culture that gives us a competitive advantage",
+				"question": '"We have a distinctive culture that gives us a competitive advantage"',
 				"options": ["Agree", "Disagree"]
 			}, 
 			{
 				"questionNo": 5,
-				"question": "People are rewarded more based on",
+				"question": '"People here are rewarded more based on..."',
 				"options": ["Performance of the specific unit", "Company performance"]
 			}, 
 			{
 				"questionNo": 6,
-				"question": "People who innovate get rewarded here",
+				"question": '"People who innovate get rewarded here"',
 				"options": ["Agree", "Disagree"]
 			}, 
 			{
 				"questionNo": 7,
-				"question": "Information flows freely across organizational boundaries",
+				"question": '"Information flows freely across organizational boundaries"',
 				"options": ["Agree", "Disagree"]
 			}, 
 			{
 				"questionNo": 8,
-				"question": "When big things change in our environment, we adapt successfully",
+				"question": '"When big things change in our environment, we adapt successfully"',
 				"options": ["Agree", "Disagree"]
 			},  
 			{
 				"questionNo": 9,
-				"question": "Leaders here are more",
+				"question": '"Leaders here are more..."',
 				"options": ["Focused on immediate objectives", "Playing for the long term"]
 			}, 
 			{
 				"questionNo": 10,
-				"question": "The organization is generally more",
+				"question": '"The organization is generally more..."',
 				"options": ["People oriented", "Task oriented"]
 			}, 
 			{
 				"questionNo": 11,
-				"question": "Our organization’s structure has",
+				"question": '"Our organization’s structure has..."',
 				"options": ["About the right number of layers", "Too many layers and too much complexity"]
 			}, 
 			{
 				"questionNo": 12,
-				"question": "People in our organization collaborate effectively across organizational lines ",
+				"question": '"People in our organization collaborate effectively across organizational lines"',
 				"options": ["Agree", "Disagree"]
 			}
 		],
